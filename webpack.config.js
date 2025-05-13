af20b3639e8557d83b4e9a0bded273b3da0dbb23
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-05-13 19:02:53 +0200

    Fix html loader, add rule to use js images

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,11 @@ module.exports = {
             },
             {
                 test: /\.html$/i,
-                use: "html-loader",
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
             },
         ],
     },
