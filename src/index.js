8586c214eba4343e2d9a85790fa24cea28cdf41b
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-05-14 11:37:11 +0200

    Implement tab navigation logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import "./styles.css";
 import { firstPage } from "./firstLoad";
 
+const content = document.querySelector("#content");
+const btnHome = document.querySelector(".btn-home");
+const btnMenu = document.querySelector(".btn-menu");
+const btnAbout = document.querySelector(".btn-about");
+
 firstPage();
+
+btnHome.addEventListener("click", () => {
+    content.innerHTML = "";
+    firstPage();
+});
+
+btnMenu.addEventListener("click", () => {
+    content.innerHTML = "";
+});
+
+btnAbout.addEventListener("click", () => {
+    content.innerHTML = "";
+});
